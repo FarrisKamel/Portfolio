cc41608fda78b6135a77285ae432445f2608d48a
Farris Kamel <farris.alqalam@gmail.com>
2025-02-27 19:16:11 -0500

    Final 1.11

--- frontend/src/data/project_data.js
+++ frontend/src/data/project_data.js
@@ -1,15 +1,15 @@
 const project_data = [
     {
+        title: "BlackRock - MSCI Index Rebalancing Price Prediction Strategy",
+        description: "Understanding price patterns and their determinants around index rebalancing  events to help improve an active trading strategy and provide value to the market by minimizing price distortion on the effective date.",
+        image: "/assets/BlackRock.png",
+        link: "https://www.blackrock.com/us/individual"
+    },
+    {
         title: "Galaxy Invaders",
         description: "This project explores 2D game development using Python and Pygame. Inspired by Space Invaders, it covers image rendering, player input handling, and maintaining a smooth 60 FPS. It also introduces the use of display and regular surfaces for efficient game rendering, laying the foundation for future projects.",
         image: "/assets/GalaxyInvaders.png", 
         link: "https://github.com/FarrisKamel/LearningToMakeAGame"
-    },
-    {
-        title: "Stock Portfolio Analysis",
-        description: "This project applies concepts from an Intro to Financial Engineering course to analyze S&P 500 stocks. Data is gathered from Wikipedia and Yahoo Finance, processed into a dataframe, and enriched with key indicators like Garman-Klass Volatility, RSI, MACD, and Dollar Volume. The top 10 stocks are selected monthly based on liquidity and performance.",
-        image: "/assets/StockPortfolioAnalysis.png", 
-        link: "https://github.com/FarrisKamel/Portfolio-analysis"
     },
     {
         title: "Real-Time Vehicle Distance Estimation",
@@ -18,11 +18,11 @@ const project_data = [
         link: "https://github.com/FarrisKamel/K-Nearest-Neighbor"
     },
     {
-        title: "Neovim Configuration",
-        description: "A central hub for storing and managing my Neovim configurations. It includes essential plugins, custom settings, and key mappings tailored to my workflow. Neovim is my preferred IDE, and I plan to use it as my primary development environment for the foreseeable future.", 
-        image: "/assets/NeovimConfig.png",
-        link: "https://github.com/FarrisKamel/Neovim-Config"
-    }, 
+        title: "Stock Portfolio Analysis",
+        description: "This project applies concepts from an Intro to Financial Engineering course to analyze S&P 500 stocks. Data is gathered from Wikipedia and Yahoo Finance, processed into a dataframe, and enriched with key indicators like Garman-Klass Volatility, RSI, MACD, and Dollar Volume. The top 10 stocks are selected monthly based on liquidity and performance.",
+        image: "/assets/StockPortfolioAnalysis.png", 
+        link: "https://github.com/FarrisKamel/Portfolio-analysis"
+    },
     {
         title: "Neural Network Dependency Parser",
         description: "A central hub for storing and managing my Neovim configurations. It includes essential plugins, custom settings, and key mappings tailored to my workflow. Neovim is my preferred IDE, and I plan to use it as my primary development environment for the foreseeable future.", 
@@ -46,6 +46,12 @@ const project_data = [
         description: "Built during my early experience with IDenTV, this program uses background threading to enable hand detection via cURL commands. Users can start and stop detection threads, each identified by a unique skuid, ensuring efficient real-time processing.",
         image: "/assets/ClearCapture.png",
         link: "https://github.com/FarrisKamel/hand_detection"
+    },
+    {
+        title: "Neovim Configuration",
+        description: "A central hub for storing and managing my Neovim configurations. It includes essential plugins, custom settings, and key mappings tailored to my workflow. Neovim is my preferred IDE, and I plan to use it as my primary development environment for the foreseeable future.", 
+        image: "/assets/NeovimConfig.png",
+        link: "https://github.com/FarrisKamel/Neovim-Config"
     }
 ]
 
